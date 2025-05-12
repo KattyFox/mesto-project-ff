13cb348bd508b21b9a64e6aafa9adbfd0bef9fd2
Ekaterina Verbilova <cverbilova@gmail.com>
2025-05-12 22:52:18 +0300

    added cards and deleted button is work

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+function createCard(cardData, deleteCallback){
+  //clone card template
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+  //detected card Elements
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    
+  
+  // fill the cards Data
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+  
+    deleteButton.addEventListener('click', function() {
+    cardElement.remove();
+  })
+  return cardElement;
+  }
+  
+  
+  const placesList = document.querySelector('.places__list');
+  
+  initialCards.forEach(card => {
+    placesList.append(createCard(card));
+  });
+  
+  
